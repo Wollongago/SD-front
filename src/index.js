3c0e06df30b89e65d1ffe218d0b1e7f5c125436f
Mak Shek Wang Phil <phineasmk@gmail.com>
2023-03-15 14:07:34 +0800

    feat: blank homepage with unresolved hook issue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navbar from './components/Navbar';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
@@ -11,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Navbar />
     <BrowserRouter>
       <App />
     </BrowserRouter>
